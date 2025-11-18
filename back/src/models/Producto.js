0ea15aa95f96e094f0074cfe22dc20e6d85c3f01
Juanks15 <duenasjkmilo1946@gmail.com>
2025-11-17 19:18:15 -0500

    Cambio y correción codigo producto

---
+++ back/src/models/Producto.js
@@ -0,0 +1,11 @@
+class Producto {
+    constructor(idProducto, nombre, precio, tipo) {
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.descripcion = "";
+        this.precio = precio;
+        this.stock = 0;
+        this.categoria = "";
+        this.tipo = tipo; 
+    }
+}
